02b4b2a4270748fca443f238cb37b6aef25d5f47
Louis Cavecchi <cavecchilouis@gmail.com>
2022-07-26 22:40:06 +0200

    Add layout system

--- apps/festivapp/pages/index.tsx
+++ apps/festivapp/pages/index.tsx
@@ -1,20 +1,57 @@
+import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import Container from '../components/Container';
 import Header from '../components/Header';
-
+import IntroductionWrapper from '../components/index/IntroductionWrapper/IntroductionWrapper';
+import { globalColors } from './_app';
+import { getLayout } from '../layouts/MenuLayout';
 const StyledPage = styled.div`
-  
-`;
 
-export function Index() {
+  h1 {
+    overflow: hidden;
+    span {
+      transform: translateY(150px);
+      font-size: 3rem;
+      display: inline-block;
+      transition: transform 1.2s cubic-bezier(.73,.01,0,1),-webkit-transform 1.2s cubic-bezier(.73,.01,0,1);
+      will-change: transform;
+    }
+  }
+`
+
+export default function Index() {
+
+  const h1 = useRef(null)
+
+  useEffect(() => {
+    window.addEventListener('load',() => {
+      const letters = 'Festivapp';
+      const lettersContainer = h1.current;
+  
+      [...letters].map((content,i) => {
+        return lettersContainer.innerHTML += `<span style="transition-delay: ${0.07 * i}s">${content}</span>`
+      })
+
+      setTimeout(() => {
+        document.querySelectorAll('h1 span').forEach((c: HTMLDivElement) => {
+          c.style.transform = "translateY(0px)"
+        })
+      }, 100)
+
+    })
+  })
+
   return (
-    <StyledPage>
-      <Header />
-      <Container>
-        <h1>Hello World</h1>
-      </Container>
-    </StyledPage>
+    <>
+      <IntroductionWrapper />
+      <StyledPage>
+        <Container>
+          <h1 ref={h1}></h1>
+          <p>Le réseau social de tous vos festivals !</p>
+        </Container>
+      </StyledPage>
+    </>
   );
 }
 
-export default Index;
+Index.getLayout = getLayout;
